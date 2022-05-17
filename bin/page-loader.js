8d8708139d5c19e4c6d39df7e987d0313557458e
Dmitriy <dima.komlyk@gmail.com>
2022-05-17 15:15:01 +0300

    Create program file configuration

---
+++ bin/page-loader.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import pageLoader from '../src/index.js';
+
+const program = new Command();
+
+program
+  .name('page-loader')
+  .description('Downloads a page from the network and puts it in the specified directory')
+  .version('0.0.1')
+  .option('-v, --version', 'output the version [version]', 'version')
+  .option('-o, --output [dir]', 'output dir', process.cwd())
+  .arguments('<pageUrl>')
+  .action((pageUrl) => {
+    pageLoader(pageUrl, program.opts().output)
+      .then(() => console.log('Page was successfully downloaded'))
+      .catch((error) => {
+        console.error(error.message);
+        process.exit(1);
+      });
+  });
+program.parse();
